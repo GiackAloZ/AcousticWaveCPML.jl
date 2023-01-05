4774401c3ee838cd20140d6d392972004de04c67
GiackAloZ <giacomo.aloisi1998@gmail.com>
2023-01-05 18:43:02 +0100

    Fix ricker doc

--- src/utils.jl
+++ src/utils.jl
@@ -59,7 +59,7 @@ end
 @doc raw"""
     rickersource1D(t::Real, t0::Real, f0::Real)    
 
-Ricker source time function for current time `t``, activation time `t0` and dominating frequency `f0`.
+Ricker source time function for current time `t`, activation time `t0` and dominating frequency `f0`.
 """
 function rickersource1D(t::Real, t0::Real, f0::Real)
     return (1 - 2 * (pi * f0 * (t - t0))^2) * exp(-((pi * f0 * (t - t0))^2))
