735905be42cdcb2752d8bdebdcdaf35ecd336928
GiackAloZ <giacomo.aloisi1998@gmail.com>
2023-01-05 12:15:37 +0100

    Fix make doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,10 @@
 using Documenter, AcousticWaveCPML
 
 makedocs(
-
+    sitename = "AcousticWaveCPML.jl",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    )
 )
 
 deploydocs(
